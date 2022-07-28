24fcfd882cd0853ca31e21bb207d443f0061af4f
Firingo <petr.havlin.0605@gmail.com>
2022-07-28 12:26:55 +0200

    4-5 Lekce - Jednoduchá úloha

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Zadej jméno:");
+string jmeno = Console.ReadLine();
+Console.WriteLine("Zadej příjmení:");
+string primeni = Console.ReadLine();
+Console.WriteLine("Zadej svůj věk:");
+string vek =  (Console.ReadLine());
+
+int vekCislo = int.Parse(vek);
+int celkovyVek = vekCislo + 1;
+string konfig = jmeno + " " + primeni;
+konfig = konfig.ToUpper();
+Console.WriteLine(konfig);
+Console.WriteLine("Za rok ti bude: " + celkovyVek);
+Console.ReadKey();
